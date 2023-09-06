a8a382a3f270f7543654c5a3ad871d554698357d
imi2080 <imi2080@gmail.com>
2023-09-06 09:10:49 +0900

    초기 커밋.

---
+++ sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define SIZE 5
+int main(void)
+{
+	int scores[SIZE] = { 11, 22, 33, 44, 55 };
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < SIZE; i++) {
+		sum += scores[i];
+		printf("scores[%d] = %d\n", i, scores[i]);
+	}
+	printf("sum of scores = %d\n", sum);
+	return 0;
+}
