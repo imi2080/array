e6afa12fc406c41aaa96202b634c47913468b929
imi2080 <imi2080@gmail.com>
2023-09-06 11:29:31 +0900

    정렬 과정 보이기

--- sort.c
+++ sort.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#define SIZE 5
+#define SIZE 10
 
 int main(void)
 {
 	int i, j;
-	int list[SIZE] = { 55, 33, 77, 11, 8 };
+	int list[SIZE] = { 22, 66, 44, 88, 99, 55, 33, 77, 11, 8 };
 
 	for (i = 0; i < SIZE; i++) {
+		system("cls");
 		for (j = 0; j < SIZE-1-i; j++) {
 			if (list[j] > list[j + 1]) {
 				int tmp = list[j];
@@ -14,6 +15,12 @@ int main(void)
 				list[j + 1] = tmp;
 			}
 		}
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < list[i]; j++)
+				printf("*");
+			printf("\n");
+		}
+		_getch();
 	}
 
 	for (i = 0; i < SIZE; i++) {
